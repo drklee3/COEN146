8477a1cde4c80e95fdfe58571ed36208ff81ba40
drklee3 <dlee3@scu.edu>
2019-01-18 15:06:34 -0800

    Complete lab2 part2

--- lab2/client.c
+++ lab2/client.c
@@ -20,57 +20,81 @@ int main (int, char *[]);
 /********************
  *	main
  ********************/
-int main (int argc, char *argv[])
-{
-	int i;
-	int sockfd = 0, n = 0;
-	char buff[1024];
-	char *p;
-	struct sockaddr_in serv_addr; 
+int main (int argc, char *argv[]) {
+	int sockfd = 0;
+	char buff[10];
+	struct sockaddr_in serv_addr;
 
-	if (argc != 2)
-	{
-		printf ("Usage: %s <ip of server> \n",argv[0]);
+	if (argc != 5) {
+		printf("Usage: %s <port> <ip of server> <input> <output> \n", argv[0]);
 		return 1;
-	} 
+	}
 
 	// set up
-	memset (buff, '0', sizeof (buff));
-	memset (&serv_addr, '0', sizeof (serv_addr)); 
+	memset(buff, '0', sizeof(buff));
+	memset(&serv_addr, '0', sizeof(serv_addr)); 
 
 	// open socket
-	if ((sockfd = socket (AF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		printf ("Error : Could not create socket \n");
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("Error : Could not create socket \n");
 		return 1;
 	} 
 
 	// set address
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons (5000); 
+	serv_addr.sin_port = htons(atoi(argv[1]));
 
-	if (inet_pton (AF_INET, argv[1], &serv_addr.sin_addr) <= 0)
-	{
-		printf ("inet_pton error occured\n");
+	if (inet_pton(AF_INET, argv[2], &serv_addr.sin_addr) <= 0) {
+		printf("inet_pton error occured\n");
 		return 1;
 	} 
 
 	// connect
-	if (connect (sockfd, (struct sockaddr *)&serv_addr, sizeof (serv_addr)) < 0)
-	{
-		printf ("Error : Connect Failed \n");
+	if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+		printf("Error : Connect Failed \n");
 		return 1;
-	} 
+	}
 
-	// input, send to server, receive it back, and output it
-	while (scanf ("%s", buff) == 1)
-	{
-		write (sockfd, buff, strlen (buff) + 1);
-		read (sockfd, buff, sizeof (buff));
-		printf ("%s\n", buff);
-	} 
+	// open file
+	FILE* fp = fopen(argv[3], "rb");
 
-	close (sockfd);
+	if (!fp) {
+		printf("Failed to open input file\n");
+		return 1;
+	}
+
+	// check if output file size is valid
+	if (strlen(argv[4]) > 9) {
+		printf("Output file name is too long (max 9)\n");
+		return 1;
+	}
+
+	// send output file name
+	write(sockfd, argv[4], strlen(argv[4]) + 1);
+
+	// receive 1
+	read(sockfd, buff, sizeof(buff));
+
+	// check if successful response
+	if (buff[0] != '1') {
+		printf("Invalid response, exiting\n");
+		return 1;
+	}
+
+	// send file
+	size_t read_bytes = 0;
+	size_t total_bytes = 0;
+	do {
+		read_bytes = fread(&buff, 1, 10, fp); // read from file
+		write(sockfd, buff, read_bytes); 	 	  // write to socket
+
+		total_bytes += read_bytes;
+	} while (read_bytes);
+
+	printf("Sent file (%zu bytes)\n", total_bytes);
+
+	fclose(fp);
+	close(sockfd);
 
 	return 0;
 
--- lab2/server.c
+++ lab2/server.c
@@ -14,19 +14,18 @@
 #include <sys/types.h>
 #include <ctype.h>
 
-int main (int, char *[]); 
+int main(int, char *[]); 
 
 
 /*********************
  * main
  *********************/
-int main (int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	int n;
-	char *p; 
+	// char *p; 
 	int listenfd = 0, connfd = 0;
 	struct sockaddr_in serv_addr; 
-	char buff[1025];
+	char buff[10];
 
 	// set up
 	memset (&serv_addr, '0', sizeof (serv_addr));
@@ -37,31 +36,37 @@ int main (int argc, char *argv[])
 	serv_addr.sin_port = htons (5000); 
 
 	// create socket, bind, and listen
-	listenfd = socket (AF_INET, SOCK_STREAM, 0);
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
 	bind (listenfd, (struct sockaddr*)&serv_addr, sizeof (serv_addr)); 
-	listen (listenfd, 10); 
+	listen (listenfd, 10);
 
 	// accept and interact
-	while (1)
-	{
-		connfd = accept (listenfd, (struct sockaddr*)NULL, NULL); 
+	while (1) {
+		connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+		printf("Accepted connection\n");
 
-		// receive data and reply
-		while ((n = read (connfd, buff, sizeof (buff))) > 0)
-		{
-			// change lower to upper case 
-			p = buff;
-			while (*p != '\0')
-			{
-				if (islower (*p))
-					*p = toupper (*p);
-				p++;
-			}
+		// read destination file
+		n = read(connfd, buff, sizeof(buff));
 
-        		write (connfd, buff, p - buff + 1); 
+		FILE* fp = fopen(buff, "wb");
+
+		// write response of "1"
+		char resp[] = {'1'};
+		write(connfd, resp, 1);
+
+		size_t written_bytes = 0;
+
+		// receive file data
+		while ((n = read(connfd, buff, sizeof(buff))) > 0) {
+			fwrite(buff, n, 1, fp);
+
+      written_bytes += n;
 		}
 
-        	close (connfd);
+		printf("Received file (%zu bytes)\n", written_bytes);
+
+		fclose(fp);
+    close (connfd);
 	}
 
 	return 0;
